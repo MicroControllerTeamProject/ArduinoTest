14641730668ad6eb677a72162cb7055931715f4e
luigisantagada <luigi.santagada@live.com>
2023-02-28 13:27:52 +0100

    .

--- ArduinoTest/ArduinoTest.ino
+++ ArduinoTest/ArduinoTest.ino
@@ -3,6 +3,7 @@
  Created:	10/24/2018 2:30:24 PM
  Author:	luigi.santagada
 */
+//Add modify by lsgsoftware
 
 // the setup function runs once when you press reset or power the board
 #include "eRCaGuy_analogReadXXbit.h"
