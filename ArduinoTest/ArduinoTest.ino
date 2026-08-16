7dc693d8203b52f1b7561e891334e6c32c9ac984
Test Developer 01 <lsgsoftware.dev01@hotmail.com>
2026-08-16 14:34:30 +0200

    remake test project

--- ArduinoTest/ArduinoTest.ino
+++ ArduinoTest/ArduinoTest.ino
@@ -1,18 +1,10 @@
-<<<<<<< HEAD
-//commento per merge 2
-=======
-//commento per merge 1
->>>>>>> 549ac6584179e32a34b705e67955e4f28fb72d70
 /*  
  Name:		ArduinoTest.ino 
  Created:	10/24/2018 2:30:24 PM
  Author:	luigi.santagada
 */
-//Add modify by lsgsoftware
 
 // the setup function runs once when you press reset or power the board
-#include "classes/MyClass.h"
-
 void setup() {
 	Serial.begin(115200);
 }
@@ -25,6 +17,7 @@ void loop() {
 int funzione_two()
 {
 	//3456....
+	return 0;
 }
 
 void funzione_one()
